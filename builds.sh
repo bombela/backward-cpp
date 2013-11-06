9b1c9563e064d6833b1c1b8efb6214fc55f6fe56
François-Xavier Bourlet <bombela@gmail.com>
2013-11-05 23:02:16 -0800

    Some improvement to builds.sh

--- builds.sh
+++ builds.sh
@@ -2,26 +2,28 @@
 
 COMPILERS_CXX98=`cat<<EOF
 gcc-4.4
-gcc-4.5
 gcc-4.6
+gcc-4.7
 gcc-4.8
 clang
 EOF`
 
 COMPILERS_CXX11=`cat<<EOF
+gcc-4.7
 gcc-4.8
 clang
 EOF`
+
 
 function mkbuild() {
 	local compiler=$1
 	local lang=$2
 	local buildtype=$3
-	local builddir="build_$1_$2"
+	local builddir="$4"
 	export CC=$compiler
 	export CXX=`echo $compiler | sed -e 's/clang/clang++/' -e 's/gcc/g++/'`
 	export CXXFLAGS="-std=$lang"
-	echo "creating $builddir"
+	echo "Creating $builddir"
 	mkdir $builddir 2>/dev/null
 	(
 		cd "$builddir"
@@ -42,43 +44,34 @@ function dotest() {
 	return 0
 }
 
-action=$1
-shift
+function do_action() {
+	local lang=$1
+	local action=$2
+	shift 2
 
-if [[ $action == "cmake" ]]; then
-	buildtype=$1
-	shift
-	for compiler in $COMPILERS_CXX98; do
-		mkbuild $compiler c++98 $buildtype
-		[[ $? != 0 ]] && exit
+	for compiler in $COMPILERS; do
+		local builddir="build_${lang}_${compiler}"
+
+		if [[ $action == "cmake" ]]; then
+			buildtype=$1
+			mkbuild $compiler $lang $buildtype $builddir
+			[[ $? != 0 ]] && exit
+		elif [[ $action == "make" ]]; then
+			build "$builddir" $@
+			[[ $? != 0 ]] && exit
+		elif [[ $action == "test" ]]; then
+			dotest "$builddir" $@
+			[[ $? != 0 ]] && exit
+		elif [[ $action == "clean" ]]; then
+			rm -r "$builddir"
+		else
+			echo "usage: $0 cmake [debug|release|relwithdbg]|make|test|clean"
+			exit 255
+		fi
 	done
-	for compiler in $COMPILERS_CXX11; do
-		mkbuild $compiler c++11 $buildtype
-		[[ $? != 0 ]] && exit
-	done
-elif [[ $action == "make" ]]; then
-	for compiler in $COMPILERS_CXX98; do
-		build "build_${compiler}_c++98" $@
-		[[ $? != 0 ]] && exit
-	done
-	for compiler in $COMPILERS_CXX11; do
-		build "build_${compiler}_c++11" $@
-		[[ $? != 0 ]] && exit
-	done
-elif [[ $action == "test" ]]; then
-	for compiler in $COMPILERS_CXX98; do
-		dotest "build_${compiler}_c++98" $@
-		[[ $? != 0 ]] && exit
-	done
-	for compiler in $COMPILERS_CXX11; do
-		dotest "build_${compiler}_c++11" $@
-		[[ $? != 0 ]] && exit
-	done
-elif [[ $action == "clean" ]]; then
-	for compiler in $COMPILERS_CXX98; do
-		rm -r "build_${compiler}_c++98"
-	done
-	for compiler in $COMPILERS_CXX11; do
-		rm -r "build_${compiler}_c++11"
-	done
-fi
+}
+
+COMPILERS=$COMPILERS_CXX98
+do_action c++98 $@
+COMPILERS=$COMPILERS_CXX11
+do_action c++11 $@
