7b3e9511efdd450982edcb99e5e763285640bb8b
Edison Gustavo Muenz <muenz@esss.com.br>
2015-08-28 18:01:53 -0300

    Compilers with GLIBC < 2.10 don't have the psiginfo() function

--- backward.hpp
+++ backward.hpp
@@ -1945,7 +1945,9 @@ private:
 		printer.address = true;
 		printer.print(st, stderr);
 
+#if _XOPEN_SOURCE >= 700 || _POSIX_C_SOURCE >= 200809L
 		psiginfo(info, 0);
+#endif
 
 		// try to forward the signal.
 		raise(info->si_signo);
