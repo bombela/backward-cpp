9fb93a08aa7e8015d7d1ef4d2d26e3073a00dd4b
François-Xavier Bourlet <fx.bourlet@rubrik.com>
2019-08-22 14:24:56 -0700

    clang-format

--- test/_test_main.cpp
+++ test/_test_main.cpp
@@ -24,8 +24,8 @@
 #include "test.hpp"
 #include <cstdio>
 #include <cstdlib>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #if defined(__has_include) && __has_include(<error.h>)
 #include <error.h>
@@ -40,113 +40,116 @@
 #endif
 
 void error(int status, int errnum, const char *format, ...) {
-	fflush(stdout);
-	fprintf(stderr, "%s: ", getprogname());
+  fflush(stdout);
+  fprintf(stderr, "%s: ", getprogname());
 
-	va_list args;
-	va_start(args, format);
-	vfprintf(stderr, format, args);
-	va_end(args);
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
 
-	if (errnum != 0) {
-		fprintf(stderr, ": %s\n", strerror(errnum));
-	} else {
-		fprintf(stderr, "\n");
-	}
-	if (status != 0) {
-		exit(status);
-	}
+  if (errnum != 0) {
+    fprintf(stderr, ": %s\n", strerror(errnum));
+  } else {
+    fprintf(stderr, "\n");
+  }
+  if (status != 0) {
+    exit(status);
+  }
 }
 #endif
 
 test::test_registry_t test::test_registry;
 using namespace test;
 
-bool run_test(TestBase& test) {
-	printf("-- running test case: %s\n", test.name);
+bool run_test(TestBase &test) {
+  printf("-- running test case: %s\n", test.name);
 
-	fflush(stdout);
-	pid_t child_pid = fork();
-	if (child_pid == 0) {
-		exit(static_cast<int>(test.run()));
-	}
-	if (child_pid == -1) {
-		error(EXIT_FAILURE, 0, "unable to fork");
-	}
+  fflush(stdout);
+  pid_t child_pid = fork();
+  if (child_pid == 0) {
+    exit(static_cast<int>(test.run()));
+  }
+  if (child_pid == -1) {
+    error(EXIT_FAILURE, 0, "unable to fork");
+  }
 
-	int child_status = 0;
-	waitpid(child_pid, &child_status, 0);
+  int child_status = 0;
+  waitpid(child_pid, &child_status, 0);
 
-	test::TestStatus status;
+  test::TestStatus status;
 
-	if (WIFEXITED(child_status)) {
-		int exit_status = WEXITSTATUS(child_status);
-		if (exit_status & ~test::STATUS_MASK) {
-			status = test::FAILED;
-		} else {
-			status = static_cast<test::TestStatus>(exit_status);
-		}
-	} else if (WIFSIGNALED(child_status)) {
-		const int signum = WTERMSIG(child_status);
-		printf("!! signal (%d) %s\n", signum, strsignal(signum));
-		switch (signum) {
-			case SIGABRT:
-				status = test::SIGNAL_ABORT; break;
-			case SIGSEGV:
-			case SIGBUS:
-				status = test::SIGNAL_SEGFAULT; break;
-			case SIGFPE:
-				status = test::SIGNAL_DIVZERO; break;
-			default:
-				status = test::SIGNAL_UNCAUGHT;
-		}
-	} else {
-		status = test::SUCCESS;
-	}
+  if (WIFEXITED(child_status)) {
+    int exit_status = WEXITSTATUS(child_status);
+    if (exit_status & ~test::STATUS_MASK) {
+      status = test::FAILED;
+    } else {
+      status = static_cast<test::TestStatus>(exit_status);
+    }
+  } else if (WIFSIGNALED(child_status)) {
+    const int signum = WTERMSIG(child_status);
+    printf("!! signal (%d) %s\n", signum, strsignal(signum));
+    switch (signum) {
+    case SIGABRT:
+      status = test::SIGNAL_ABORT;
+      break;
+    case SIGSEGV:
+    case SIGBUS:
+      status = test::SIGNAL_SEGFAULT;
+      break;
+    case SIGFPE:
+      status = test::SIGNAL_DIVZERO;
+      break;
+    default:
+      status = test::SIGNAL_UNCAUGHT;
+    }
+  } else {
+    status = test::SUCCESS;
+  }
 
-	if (test.expected_status == test::FAILED) {
-		return (status & test::FAILED);
-	}
+  if (test.expected_status == test::FAILED) {
+    return (status & test::FAILED);
+  }
 
-	if (test.expected_status == test::SIGNAL_UNCAUGHT) {
-		return (status & test::SIGNAL_UNCAUGHT);
-	}
+  if (test.expected_status == test::SIGNAL_UNCAUGHT) {
+    return (status & test::SIGNAL_UNCAUGHT);
+  }
 
-	return status == test.expected_status;
+  return status == test.expected_status;
 }
 
-int main(int argc, const char* const argv[]) {
+int main(int argc, const char *const argv[]) {
 
-	size_t success_cnt = 0;
-	size_t total_cnt = 0;
-	for (test_registry_t::iterator it = test_registry.begin();
-			it != test_registry.end(); ++it) {
-		TestBase& test = **it;
+  size_t success_cnt = 0;
+  size_t total_cnt = 0;
+  for (test_registry_t::iterator it = test_registry.begin();
+       it != test_registry.end(); ++it) {
+    TestBase &test = **it;
 
-		bool consider_test = (argc <= 1);
-		for (int i = 1; i < argc; ++i) {
-			if (strcasecmp(argv[i], test.name) == 0) {
-				consider_test = true;
-				break;
-			}
-		}
-		if (not consider_test) {
-			continue;
-		}
+    bool consider_test = (argc <= 1);
+    for (int i = 1; i < argc; ++i) {
+      if (strcasecmp(argv[i], test.name) == 0) {
+        consider_test = true;
+        break;
+      }
+    }
+    if (not consider_test) {
+      continue;
+    }
 
-		total_cnt += 1;
-		if (run_test(test)) {
-			printf("-- test case success: %s\n", test.name);
-			success_cnt += 1;
-		} else {
-			printf("** test case FAILED : %s\n", test.name);
-		}
-	}
-	printf("-- tests passing: %lu/%lu", success_cnt, total_cnt);
-	if (total_cnt) {
-		printf(" (%lu%%)\n", success_cnt * 100 / total_cnt);
-	} else {
-		printf("\n");
-	}
-	return (success_cnt == total_cnt) ? EXIT_SUCCESS : EXIT_FAILURE;
+    total_cnt += 1;
+    if (run_test(test)) {
+      printf("-- test case success: %s\n", test.name);
+      success_cnt += 1;
+    } else {
+      printf("** test case FAILED : %s\n", test.name);
+    }
+  }
+  printf("-- tests passing: %lu/%lu", success_cnt, total_cnt);
+  if (total_cnt) {
+    printf(" (%lu%%)\n", success_cnt * 100 / total_cnt);
+  } else {
+    printf("\n");
+  }
+  return (success_cnt == total_cnt) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
